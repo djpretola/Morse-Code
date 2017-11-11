d380378153d4c1e5ba0a6238513de469c25b17f0
David Pretola <djpretola@users.noreply.github.com>
2017-11-11 10:56:59 -0500

    Added the project license: GNU GENERAL PUBLIC LICENSE version 3.

--- Arduino/Morse/Morse.ino
+++ Arduino/Morse/Morse.ino
@@ -1,4 +1,19 @@
 /*
+Copyright (C) 2017 David Pretola
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
  * A simple sketch to allow for the input of ASCII text,
  * via serial, it's conversion to Morse Code and finally
  * sending that code via the onboard LED light, a Speaker
